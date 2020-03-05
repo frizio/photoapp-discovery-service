518c8781ab65d8a8c6a2f7330c21267972907981
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2020-03-05 17:02:14 +0100

    first commit

---
+++ src/test/java/cloud/frizio/ms/photoapp/photoappdiscoveryservice/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.frizio.ms.photoapp.photoappdiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
