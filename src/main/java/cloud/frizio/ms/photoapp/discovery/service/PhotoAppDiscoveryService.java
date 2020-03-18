270da9a59e5ccb1a6479a80e3d0b9e9afa0cf671
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2020-03-18 18:25:47 +0100

    Refactor code

--- src/main/java/cloud/frizio/ms/photoapp/discovery/service/PhotoAppDiscoveryService.java
+++ src/main/java/cloud/frizio/ms/photoapp/discovery/service/PhotoAppDiscoveryService.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class DiscoveryServiceApp {
+public class PhotoAppDiscoveryService {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DiscoveryServiceApp.class, args);
+		SpringApplication.run(PhotoAppDiscoveryService.class, args);
 	}
 
 }
--- src/test/java/cloud/frizio/ms/photoapp/discovery/service/PhotoAppDiscoveryServiceTests.java
+++ src/test/java/cloud/frizio/ms/photoapp/discovery/service/PhotoAppDiscoveryServiceTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DiscoveryServiceAppTests {
+class PhotoAppDiscoveryServiceTests {
 
 	@Test
 	void contextLoads() {
