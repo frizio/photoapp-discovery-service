918ec0753a39074a45818aca3aeed8834d094d40
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2020-03-18 18:34:37 +0100

    Refactor code

--- src/test/java/cloud/frizio/ms/photoapp/discovery/service/DiscoveryServiceTests.java
+++ src/test/java/cloud/frizio/ms/photoapp/discovery/service/DiscoveryServiceTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PhotoAppDiscoveryServiceTests {
+class DiscoveryServiceTests {
 
 	@Test
 	void contextLoads() {
